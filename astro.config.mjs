23cfcee1b132ef91b1ec48d0a6f69eca1dd567e0
NotThatNinja <samuele8max@gmail.com>
2025-08-12 23:56:54 +0200

    Actually removed i18n, made changes to info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: ["en", "it", "et", "es", "fr"],
-        defaultLocale: "en",
-    }
 });

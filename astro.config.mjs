1375369427e93520c213d5d26a2ebb458edf9efe
NotThatNinja <samuele8max@gmail.com>
2025-08-15 12:25:49 +0200

    Added sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://www.vxit.io',
     i18n: {
         locales: ["en", "it", "es", "et", "fr", "de"],
         defaultLocale: "en",
         routing: {
             prefixDefaultLocale: true
         }
-    }
+    },
+
+    integrations: [sitemap()]
 });
